ca8aa599811d79aa95d940c3678ba802ee7c4d53
flow-state-15 <purcellgroup88@gmail.com>
2021-09-17 16:44:19 -0500

    added seed file

---
+++ seed-data.js
@@ -0,0 +1,117 @@
+const
+
+const seedData = [
+    {
+        name: "Intellexer",
+        url: "http://esapi.intellexer.com/?ref=apilist.fun",
+        free: false,
+        description: "Intellexer API is a cloud service that enables developers to embed natural language processing and text analysis tools in consumer and enterprise applications, or web-services using JSON or XML. Intellexer API includes natural language processing solutions for sentiment analysis, entity recognition, summarization, document comparison, natural language interface for search engines, language detection, spell-checking, etc. Intellexer API methods can be called from any programming language or software component that supports HTTP requests.",
+        logo_url: "https://res.cloudinary.com/dan-purcell-2021/image/upload/v1631912855/aceapi-assets/Intellexer_giczuh.png",
+        tags_id: "1",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "API2Cart",
+        url: "https://api2cart.com/",
+        free: false,
+        description: "API2Cart is a unified API to integrate your eCommerce software with 40+ shopping platforms like Magento, Shopify, eBay, etc. We provide SDK and detailed documentation to help you connect multiple shopping carts and marketplaces in one go. Work with all needed data from your clients' stores like products, orders, shipments, customers and baskets. Integrate your SaaS app startup with Magento, Shopify, WooCommerce and other shopping platforms that we support at once.",
+        logo_url: "https://res.cloudinary.com/dan-purcell-2021/image/upload/v1631912747/aceapi-assets/API2Cart_zzmvet.png",
+        tags_id: "2",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "stream",
+        url: "https://getstream.io/",
+        free: false,
+        description: "The #1 Chat API for Custom Messaging Apps. Build real-time chat messaging in less time. Rapidly ship in-app messaging with our highly reliable chat infrastructure and feature-rich SDKs. Drive in-app conversion, engagement, and retention. Our enterprise-ready activity feed APIs are used by hundreds of large companies so they can grow without worrying about the scalability, maintenance and reliability of a complicated news feed infrastructure.",
+        logo_url: "https://res.cloudinary.com/dan-purcell-2021/image/upload/v1631913638/aceapi-assets/stream_aosgw7.png",
+        tags_id: "3",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "ScrapingBee",
+        url: "https://www.scrapingbee.com/",
+        free: false,
+        description: "Tired of getting blocked while scraping the web? ScrapingBee API handles headless browsers and rotates proxies for you. Thanks to our large proxy pool, you can bypass rate limiting website, lower the chance to get blocked and hide your bots! For SEO, keyword monitoring or backlink checking. Scraping search engine result pages is extremely painful because of rate limits. ScrapingBee works great for general web scraping tasks like real estate scraping, price-monitoring, extracting reviews without getting blocked.",
+        logo_url: "https://res.cloudinary.com/dan-purcell-2021/image/upload/v1631914029/aceapi-assets/ScrapingBee_wu3a0b.png",
+        tags_id: "4",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "PDFShift",
+        url: "https://pdfshift.io/",
+        free: false,
+        description: "Stop wasting time implementing and maintaining a third-party software/library. With PDFShift, rely on an up-to-date, high-fidelity conversion API with no maintenance costs.",
+        logo_url: "https://res.cloudinary.com/dan-purcell-2021/image/upload/v1631914142/aceapi-assets/PDFShift_elrovc.png",
+        tags_id: "",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "",
+        url: "",
+        free: false,
+        description: "",
+        logo_url: "",
+        tags_id: "",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+]
+
+const tags = [
+    {
+        name: "Artificial Intelligence--1",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "eCommerce--2",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Chat Technology--3",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Web Scraping--4",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence--5",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence--6",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence--7",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence--8",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence--9",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        name: "Artificial Intelligence-10",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+]
