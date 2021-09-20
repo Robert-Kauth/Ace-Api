b2cc9e4402e97b71b26b30f179f69283ccb7b6c7
flow-state-15 <purcellgroup88@gmail.com>
2021-09-20 15:11:07 -0500

    added login routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async(req, res, next) => {
   res.render('index', { title: 'a/A Express Skeleton Home' });
+});
+
+router.get('/login', csrfProtection, async(req, res, next) => {
+  res.render('login', { csrfToken: req.csrfToken() });
+});
+
+router.get('/signup', csrfProtection, async(req, res, next) => {
+  res.render('signup', { csrfToken: req.csrfToken()  });
 });
 
 module.exports = router;
