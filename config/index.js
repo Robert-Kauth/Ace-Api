6184632cdc3f2713f13da05fe43805f20607dbd0
Jack Percival <jackpercival7@gmail.com>
2021-09-20 13:50:28 -0700

    Add initial db index

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
   db: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: "aceapi_app",
+    password: "password",
+    database: "aceapi_dev",
+    host: "localhost",
   },
 };
