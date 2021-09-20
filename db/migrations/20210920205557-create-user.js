56ebb70b53e88c57a1a992cf7844cd7aac679d6d
Jack Percival <jackpercival7@gmail.com>
2021-09-20 14:03:47 -0700

    Add user table,migrations,model

---
+++ db/migrations/20210920205557-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      last_name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(255)
+      },
+      hashed_password: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ db/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    first_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true
+    },
+    hashed_password: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
