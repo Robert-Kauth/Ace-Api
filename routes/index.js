2c52258c21733c488be3fc0e333b7f83d8e6ce84
Jack Percival <jackpercival7@gmail.com>
2021-09-20 14:20:23 -0700

    User signup sorta done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -10,16 +11,33 @@ router.get('/', async(req, res, next) => {
 });
 
 router.get('/login', csrfProtection, async(req, res, next) => {
-  res.render('login', { csrfToken: req.csrfToken() });
+  const user = db.User.build();
+  res.render('login', { title: "Ace API - Login", user, csrfToken: req.csrfToken() });
 });
 
 router.get('/signup', csrfProtection, async(req, res, next) => {
-  res.render('signup', { csrfToken: req.csrfToken()  });
+  const user = db.User.build();
+  res.render('signup', { title: "Ace API - Sign Up", user, csrfToken: req.csrfToken()  });
 });
 
 router.post('/signup', csrfProtection, asyncHandler (async(req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+  } = req.body;
 
-  res.render('signup', { csrfToken: req.csrfToken()  });
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await db.User.create({
+    firstName,
+    lastName,
+    email,
+    hashedPassword
+  });
+  console.log("User created!!!!")
+  res.redirect('/');
 }));
 
 module.exports = router;
