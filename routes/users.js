168db1d48be24fd9cec45f81fe1e696db1adefb5
Robert Kauth <kauthr@gmail.com>
2021-09-21 14:12:46 -0700

    added db query for toolboxes and imported models

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { User } = require('../db/models');
+const { User, Toolbox, Implementation, Api } = require('../db/models');
 const { asyncHandler } = require('../utils');
 const { restoreUser, requireAuth, authorize } = require('../auth');
 
@@ -13,11 +13,12 @@ router.get(
 	authorize,
 	asyncHandler(async (req, res, next) => {
 		const user = await User.findByPk(req.params.id);
+		const toolboxes = await Toolbox.findAll();
 		if (user) {
 			res.render('home', { title: 'ACEAPI' });
 		} else {
-      next()
-    }
+			next();
+		}
 	})
 );
 
