cce39733c07310c5175ca2cdc57b1c27aacc0522
Jack Percival <jackpercival7@gmail.com>
2021-09-21 12:06:45 -0700

    started the api/:id route

---
+++ routes/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const db = require('../db/models');
+const { asyncHandler } = require('../utils');
+const router = express.Router();
+
+router.get('/:id(\\d+)', asyncHandler( async (req, res, next) => {
+    //Grab the apiId
+    const api_id = req.params.id;
+
+    //Search for the specific API
+    const api = await db.Api.findByPk(api_id, {include: { model: db.Tag }});
+
+    //Get the logged in user, if they exist
+    const user_id = req.session.auth.userId;
+
+    //Get the user's toolboxes
+    const toolboxes = await db.Toolbox.findAll({
+        where: {
+            user_id
+        }
+    })
+
+    //Render the page if it exists
+    if (api) {
+        return res.render('api', {title: `Ace API - ${api.name}`, api, toolboxes})
+    } else {
+        next()
+    }
+}))
+
+
+module.exports = router;
