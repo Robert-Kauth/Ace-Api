487d9909acec4eecb99951785338777cd78988ca
Robert Kauth <kauthr@gmail.com>
2021-09-21 14:02:16 -0700

    added authorize MW to auth, started user/:id route, created home pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
+
+const { User } = require('../db/models');
+const { asyncHandler } = require('../utils');
+const { restoreUser, requireAuth, authorize } = require('../auth');
+
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', async(req, res, next) => {
-  res.send('respond with a resource');
-});
+/* GET users home page. */
+router.get(
+	'/:id(\\d+)',
+	requireAuth,
+	authorize,
+	asyncHandler(async (req, res, next) => {
+		const user = await User.findByPk(req.params.id);
+		if (user) {
+			res.render('home', { title: 'ACEAPI' });
+		} else {
+      next()
+    }
+	})
+);
 
 module.exports = router;
